250d0331d39b11d0c94829a2f27f36dbd5210749
Mobby <mobby.hytale@gmail.com>
2026-03-22 14:25:55 +0100

    DLL now wait for the game to be fully loaded to init loader

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <map>
 #include <string>
 #include "Scanner.h"
-#include "minhook/MinHook.h"
 #include "SDK.h"
 #include "Utils.h"
 
@@ -17,56 +16,50 @@ extern "C" {
 
 namespace fs = std::filesystem;
 
-void __fastcall hkProcessEvent(UObject* pObject, UFunction* pFunction, void* pParams) {
-	static bool firstCall = true;
-	if (firstCall) {
-		std::cout << "[Tavern] Hook active" << std::endl;
-		firstCall = false;
+bool IsGameFullyLoaded() {
+	if (!GObjects) return false;
+	for (int i = 0; i < GObjects->NumElements; i++) {
+		UObject* obj = GObjects->GetObjectById(i);
+		if (!obj || (uintptr_t)obj < 0x10000) continue;
+		std::string name = GetName(obj);
+		if (name.find("UMG_TitleScreen") != std::string::npos &&
+			name.find("Default__") == std::string::npos)
+			return true;
 	}
-	oProcessEvent(pObject, pFunction, pParams);
-}
-
-void InitializeModLoader() {
-	std::cout << "[Tavern] Initializing" << std::endl;
-
-	uintptr_t addrTS = Scanner::FindPattern("48 89 5C 24 08 57 48 83 EC 30 83 79 04 00");
-	if (addrTS) FNameToString = (tFNameToString)addrTS;
-
-	uintptr_t addrGE = Scanner::FindPattern("48 8B 05 ? ? ? ? 48 8B 88 ? ? ? ? 48 85 C9 74 05");
-	if (addrGE) {
-		int32_t offset = *(int32_t*)(addrGE + 3);
-		uintptr_t* pGEngine = *(uintptr_t**)(addrGE + 7 + offset);
-		if (pGEngine) {
-			uintptr_t realProcessEvent = (*(uintptr_t**)pGEngine)[64];
-			if (MH_Initialize() == MH_OK) {
-				MH_CreateHook((LPVOID)realProcessEvent, (LPVOID)&hkProcessEvent, (LPVOID*)&oProcessEvent);
-				MH_EnableHook((LPVOID)realProcessEvent);
-			}
-		}
-	}
-
-	uintptr_t addrGObj = Scanner::FindPattern("48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1");
-	if (addrGObj) {
-		int32_t offset = *(int32_t*)(addrGObj + 3);
-
-		GObjects = (TUObjectArray*)(addrGObj + 7 + offset);
-
-		std::cout << "[Tavern] GObjects found at: 0x" << std::hex << GObjects << std::endl;
-		std::cout << "[Tavern] Object count: " << std::dec << GObjects->NumElements << std::endl;
-	}
-
-	LuaRegister();
-
-	std::cout << "[Tavern] Lua engine ready" << std::endl;
+	return false;
 }
 
 DWORD WINAPI MainThread(LPVOID lpParam) {
 	AllocConsole();
 	FILE* f; freopen_s(&f, "CONOUT$", "w", stdout);
 
-	InitializeModLoader();
-	LoadLuaMods();
+	std::cout << "[Tavern] Initializing" << std::endl;
 
+	uintptr_t addrTS = Scanner::FindPattern("48 89 5C 24 08 57 48 83 EC 30 83 79 04 00");
+	if (!addrTS) {
+		std::cout << "[Tavern] Error: FNameToString not found" << std::endl;
+		FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), 0);
+		return 0;
+	}
+	FNameToString = (tFNameToString)addrTS;
+
+	uintptr_t addrGObj = Scanner::FindPattern("48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1");
+	if (!addrGObj) {
+		std::cout << "[Tavern] Error: GObjects not found" << std::endl;
+		FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), 0);
+		return 0;
+	}
+	int32_t relOffset = *(int32_t*)(addrGObj + 3);
+	GObjects = (TUObjectArray*)(addrGObj + 7 + relOffset);
+
+	std::cout << "[Tavern] Waiting for the game ..." << std::endl;
+	while (!IsGameFullyLoaded()) Sleep(500);
+	std::cout << "[Tavern] Game ready (" << std::dec << GObjects->NumElements << " objects)" << std::endl;
+
+	LuaRegister();
+	std::cout << "[Tavern] Lua engine ready" << std::endl;
+
+	LoadLuaMods();
 	std::cout << "[Tavern] Mod loader ready" << std::endl;
 
 	while (!(GetAsyncKeyState(VK_END) & 1)) {
